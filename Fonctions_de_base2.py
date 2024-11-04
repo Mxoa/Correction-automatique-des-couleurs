3ead7295be3c87e0e67dc7e8e279d6e094e94630
moncef jabal <moncef.jabal@telecom-paris.fr>
2024-11-04 21:24:04 +0100

    fonctions de base pour la deuxième methode

--- Fonctions_de_base2.py
+++ Fonctions_de_base2.py
@@ -1,9 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from numpy.polynomial.chebyshev import Chebyshev
 import scipy
 import scipy.signal
-import Fonctions_de_bases
 
 
 def saturation2(t, alpha=2):
@@ -12,20 +10,40 @@ def saturation2(t, alpha=2):
 
 x=np.linspace(-256,256,10000)
 
-def Approximation_polynomiale(f,degree,alpha=2):
-    #approximation par les polynomes de Tchebychev
-    y=f(x,alpha)
+#Première methode
+def Approximation_polynomiale(f, degree, alpha=2):
+    # Approximation par les polynômes de Tchebychev
+    y = f(x, alpha)
     cheb_poly = Chebyshev.fit(x, y, degree)
-    Max_error=np.max(np.abs(np.subtract(y,cheb_poly(x))))
-    return (cheb_poly,Max_error)
+    #max_error = np.max(np.abs(y - cheb_poly(x)))
+    coefficients = cheb_poly.coef.tolist()
+    return coefficients
 
-'''Y=Approximation_polynomiale(saturation2,17)[0](x)
-#L'erreur maximale
-print(Approximation_polynomiale(saturation2,17)[1])
-plt.plot(x,Y)
-plt.show()'''
 
-def R_interpolation1(L,img,fonction,omega,alpha=2):
+def R_poly(img, f, degree, omega, alpha=2):
+    height,width=np.size(img,0),np.size(img,1)
+    R=np.zeros(height,width)
+    Omega=np.zeros(height,width)
+    cheby=Approximation_polynomiale(f, degree, alpha)
+    for x in range(height):
+        for y in range(width):
+            Omega[x,y]=omega(x,y,0,0)
+    for x in range(height):
+        for y in range(width):
+            for i  in range(degree+1):
+                convolved_image=scipy.signal.convolve2d(Omega,img**i)
+                ai=0
+                # a c'est a.n(x) dans l'article2
+                for j in range(i,degree+1):
+                    a+=cheby[j]*((-1)**(j-i+1))*((img[x,y])**(j-i))
+                R[x,y]+=ai*convolved_image[x,y]
+    return R
+    
+
+
+
+#Deuxième methode
+def R_convolution(L,img,fonction,omega,alpha=2):
     #C'est le calcul de R(x,Lj) qu'on retrouve dans l'article 2 sauf que là on calcule R pour toute l'image au lieu d'un seul pixel x
     height,width=np.size(img,0),np.size(img,1)
     F=np.zeros((height,width))
@@ -36,7 +54,7 @@ def R_interpolation1(L,img,fonction,omega,alpha=2):
             Omega[i,j]=omega(0,0,i,j)
     return scipy.signal.convolve2d(Omega,F,mode='same')    
     
-def R_interpolation2(img,fonction,omega,number_of_levels=8,alpha=2):
+def R_interpolation(img,fonction,omega,number_of_levels=8,alpha=2):
     #C'est le calcul du developpement limité qui se trouve juste après dans le meme article
     height,width=np.size(img,0),np.size(img,1)
     L=np.zeros(number_of_levels)
@@ -47,7 +65,7 @@ def R_interpolation2(img,fonction,omega,number_of_levels=8,alpha=2):
     # modified image c'est l'image résultante
     for j in range(number_of_levels):
         L[j]=np.min(img)-(np.max(img)-np.min(img))*(j/(number_of_levels-1))
-        R.append(R_interpolation1(L[j],img,fonction,omega,alpha))
+        R.append(R_convolution(L[j],img,fonction,omega,alpha))
     for k in range(height):
         for l in range(width):
             for j in range(number_of_levels-1):
